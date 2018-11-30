e313f6a5121c12f5a660395e63b40fb1b8d6ae04
alanharon <alanharon@alans-mbp.gsu.edu>
2018-11-29 22:59:35 -0500

    Created JavaFX fxml files, login screen

--- CIS_Project/src/Login.java
+++ CIS_Project/src/Login.java
@@ -1,9 +1,28 @@
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class Login {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@FXML
+	private Label lblLogin;
+	
+	@FXML
+	private TextField txtUsername;
+	
+	@FXML
+	private TextField txtPassword;
+	
+	public void Login(ActionEvent event) {
+		
+		if(txtUsername.getText().equals("user") && txtPassword.getText().equals("pass")) {
+			lblLogin.setText("Login Success");
+		}
+		else {
+			lblLogin.setText("Login Failed");
+			
+		}
 	}
-
+		
 }
