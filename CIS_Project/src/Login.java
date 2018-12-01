1205ab5a420d15c69db52209b491a9a5540dafc2
alanharon <alanharon@alans-mbp.gsu.edu>
2018-12-01 18:42:51 -0500

    login update

--- CIS_Project/src/Login.java
+++ CIS_Project/src/Login.java
@@ -1,7 +1,17 @@
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 
 public class Login {
 
@@ -14,14 +24,43 @@ public class Login {
 	@FXML
 	private TextField txtPassword;
 	
-	public void Login(ActionEvent event) {
+	public void Login(ActionEvent event) throws Exception{
 		
-		if(txtUsername.getText().equals("user") && txtPassword.getText().equals("pass")) {
-			lblLogin.setText("Login Success");
+		if(txtUsername.getText().equals("admin") && txtPassword.getText().equals("CIS3270")) {
+			lblLogin.setText("Admin Login Success");
+			Stage primaryStage = new Stage();
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		else if (txtUsername.getText().length() == 0 || txtPassword.getText().length() == 0) {
+			
+			
 		}
 		else {
 			lblLogin.setText("Login Failed");
 			
+		}
+	}
+	
+	public void User() {
+		
+
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys", "root", "AppDevCIS2018");
+			
+			Statement statement = connection.createStatement();
+			
+			ResultSet result = statement.executeQuery("SELECT * FROM sys.User");
+			
+			while (result.next())
+			{
+				String x = result.getString("username"); 
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
 		}
 	}
 		
